d2f628c27431b4c57e4db1ac3b9453dedddf8f89
ccantu20 <carloscq920@gmail.com>
2021-05-14 16:17:21 -0500

    Added more styling to the menu items, mainly hover effects

--- client/src/components/MenuItem/MenuItem.js
+++ client/src/components/MenuItem/MenuItem.js
@@ -3,9 +3,11 @@ import './MenuItem.scss'
 
 function MenuItem({title, imageUrl, size}) {
     return (
-        <div style={{
-            backgroundImage: `url(${imageUrl})`
-        }} className={`${size} menuItem`}>
+        <div className={`${size} menuItem`}>
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">Shop Now</span>
