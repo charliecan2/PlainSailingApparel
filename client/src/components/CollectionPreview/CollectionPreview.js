dc77bcc2396351b0e8d834c3788524616e7a2522
ccantu20 <carloscq920@gmail.com>
2021-05-21 18:17:51 -0500

    Styled the preview page to have the image of the item alongside the name and price. Items are also neatly organized now

--- client/src/components/CollectionPreview/CollectionPreview.js
+++ client/src/components/CollectionPreview/CollectionPreview.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './CollectionPreview.scss'
 
+import CollectionItem from '../CollectionItem/CollectionItem';
+
 function CollectionPreview({title, items}) {
     return (
         <div className='collectionPreview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
-                {items.filter((item, idx) => idx < 4).map((item) => {
-                    return <div key={item.id}>{item.name}</div>
+                {items
+                .filter((item, idx) => idx < 4)
+                .map(({ id, ...otherItemProps }) => {
+                    return <CollectionItem key={id} {...otherItemProps} />
                 })
                 }
             </div>
